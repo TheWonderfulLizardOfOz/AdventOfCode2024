c17e33a76c1b9a3d617c327c3a90e3a5ea523f84
Garance AR <79841946+TheWonderfulLizardOfOz@users.noreply.github.com>
2024-12-01 05:23:17 +0000

    Day1

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,45 +1,32 @@
 import re
 import math
 
+lines = open("input.txt", "r", encoding="utf-8").read().splitlines()
+
+list1 = []
+list2 = []
+
+for line in lines:
+    l = line.split()
+    list1.append(int(l[0]))
+    list2.append(int(l[1]))
+
+list1 = sorted(list1)
+list2 = sorted(list2)
+
 def day1p1():
-    list1 = []
-    list2 = []
-
-    for line in lines:
-        l = line.split()
-        list1.append(int(l[0]))
-        list2.append(int(l[1]))
-
-    list1 = sorted(list1)
-    list2 = sorted(list2)
-
     tot = 0
-    for i in range(len(list1)):
-        tot += abs(list1[i] - list2[i])
+    for n1, n2 in zip(list1, list2):
+        tot += abs(n1 - n2)
 
     return tot
 
+
 def day1p2():
-    idDict = {}
-
-    list2 = []
-
-    for line in lines:
-        l = line.split()
-        if l[0] not in idDict:
-            idDict[l[0]] = 0
-        list2.append(l[1])
-
-    for num in list2:
-        if num in idDict:
-            idDict[num] += 1
-
-    total = 0
-    for num in idDict:
-        total += int(num) * idDict[num]
-
-    return total
+    tot = 0
+    for num in list1:
+        tot += num * list2.count(num)
+    return tot
 
 
-lines = open("test.txt", "r", encoding = "utf-8").read().splitlines()
 print(day1p2())
